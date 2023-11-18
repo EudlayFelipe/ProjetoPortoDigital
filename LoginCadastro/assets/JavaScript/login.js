46910267f8b3739647fe4abca55641a0571cbe37
Levi Dórea <levimota0001@gmail.com>
2023-11-18 17:01:23 -0300

    fix link pós login

--- LoginCadastro/assets/JavaScript/login.js
+++ LoginCadastro/assets/JavaScript/login.js
@@ -46,7 +46,7 @@ function entrar() {
   })
 
   if (usuario.value == userValid.user && senha.value == userValid.senha) {
-    window.location.href = '/index.html'
+    window.location.href = '/ProjetoPortoDigital/Cobranca/cobranca.html'
 
     let mathRandom = Math.random().toString(16).substr(2)
     let token = mathRandom + mathRandom
