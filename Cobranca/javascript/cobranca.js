24adb3cd2414ac80f86ca80ef4a9d249a2856f1c
Jairo Williams <jairo.neto.dev@gmail.com>
2023-11-26 13:09:14 -0300

    upd modal card and new functions for 'reset' forms and modal

--- Cobranca/javascript/cobranca.js
+++ Cobranca/javascript/cobranca.js
@@ -1,11 +1,11 @@
 // Lendo o HTML
 $(document).ready(function () {
     // Colocando as máscaras
-    $("#cardNumber").mask("0000 0000 0000 0000");
-    $("#expireDate").mask("00/0000");
-    $("#cvc_number").mask("000");
+    $("#cardNumber_input").mask("0000 0000 0000 0000");
+    $("#expireDate_input").mask("00/0000");
+    $("#cvc_number_input").mask("000");
     $("#cpf_input").mask("000.000.000-00");
-    $("#tel_input").mask("(00) 0 0000-0000")
+    $("#tel_input").mask("(00) 0 0000-0000");
 
 
     $(".filter_input").on("change", function (e) {
@@ -20,7 +20,6 @@ $(document).ready(function () {
         payf = $("#select-payment").val();
         if (payf == "") {
             error++;
-
         }
 
 
@@ -31,7 +30,7 @@ $(document).ready(function () {
         }
     });
 
-
+    // função para descer ou subir o form do cartão de crédito
     $("#select-payment").on("change", function (e) {
         value = $(this).val();
         if (value == '3') {
@@ -42,10 +41,55 @@ $(document).ready(function () {
     });
 
     // funcão para desabiliar o botão caso refaça o form
-    $("#reset_form").on("click", function(e){
-
+    $("#btn-new-cobranca").on("click", function (e) {
+        apagarInputs();
+        limparModal();
+        $("#gerar_cobranca").slideDown();
         $("#gerar_cobranca").addClass("disabled");
-    })
+        $("#btn-new-cobranca").text("Refazer")
+
+    });
+
+    // função para "limpar" meu modal após clicar em "Cancelar" ou no "X"
+    $(".btn-reset-modal").on("click", function(e){
+        limparModal();
+    });
+
+    // funcão para resetar todos os campos após clicar em "Enivar cobrança"
+    $("#liveAlertBtn").on("click", function (e) {
+        $("#liveAlertPlaceholder").slideDown();
+        $("#liveAlertBtn").addClass("disabled");
+        $("#btn-new-cobranca").text("Nova cobrança")
+        $("#btn-cancel-modal").text("Fechar")
+    });
+
+    // função para "limpar" meu modal 
+    function limparModal(){
+        $("#liveAlertPlaceholder").slideUp();
+        $("#liveAlertBtn").slideDown();
+        $("#liveAlertBtn").removeClass("disabled");
+        $("#pix_div").slideUp();
+        $("#boleto_div").slideUp();
+        $("#card_div").slideUp();
+    };
+    
+    // Função para apagar os campos após o usuário enviar a cobrança
+    function apagarInputs() {
+        $("#nome_input").val("");
+        $("#email_input").val("");
+        $("#cpf_input").val("");
+        $("#tel_input").val("");
+        $("#valor_input").val("");
+        $("#select_payment").val("");
+        $("#description_input").val("");
+
+        $("#nome_credit_input").val("");
+        $("#cardNumber_input").val("");
+        $("#expireDate_input").val("");
+        $("#cvc_number_input").val("");
+        $(".filter_input").removeClass("is-valid");
+    };
+
 
     $("#gerar_cobranca").on("click", function (e) {
 
@@ -55,7 +99,7 @@ $(document).ready(function () {
         cpf_input = $("#cpf_input").val();
         tel_input = $("#tel_input").val();
         valor_input = $("#valor_input").val();
-        input_description = $("#input-description").val();
+        description_input = $("#description_input").val();
         payment_form = $("#select-payment").val();
 
         error = false;
@@ -92,10 +136,10 @@ $(document).ready(function () {
             $("#valor_input").addClass("is-valid");
         }
 
-        if (input_description == "") {
-            $("#input_description").addClass("is-invalid");
+        if (description_input == "") {
+            $("#description_input").addClass("is-invalid");
         } else {
-            $("#input_description").addClass("is-valid");
+            $("#description_input").addClass("is-valid");
         }
 
         if (payment_form == "") {
@@ -117,7 +161,7 @@ $(document).ready(function () {
                     $("#name_note").text(nome_input);
                     $("#email_note").text(email_input);
                     $("#cpf_note").text(cpf_input);
-                    $("#desc_note").text(input_description);
+                    $("#desc_note").text(description_input);
                     $("#card_div").slideDown();
                     $(".modal-title").text("Cobrança via Cartão de Crédito")
                     break;
@@ -132,22 +176,23 @@ $(document).ready(function () {
 
 });
 
+// Scrip do bootstrap para fazer o "alert"
 const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
 const appendAlert = (message, type) => {
-  const wrapper = document.createElement('div')
-  wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    '</div>'
-  ].join('')
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        // '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
 
-  alertPlaceholder.append(wrapper)
+    alertPlaceholder.append(wrapper)
 }
 
 const alertTrigger = document.getElementById('liveAlertBtn')
 if (alertTrigger) {
-  alertTrigger.addEventListener('click', () => {
-    appendAlert('Cobrança enviada com sucesso!', 'success')
-  })
+    alertTrigger.addEventListener('click', () => {
+        appendAlert('Cobrança enviada com sucesso!', 'success')
+    })
 }   
